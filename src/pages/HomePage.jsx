2d11490589cf42d88ef7eb9debecaa887c5144d5
HikariNM <nicole.hikari.mascellino@gmail.com>
2026-03-29 18:28:36 +0200

    added path to archive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,9 @@ export default function HomePage() {
     return (
         <div className="text-center max-w-6xl mx-auto m-6">
             <h1 className="text-3xl pb-2">Welcome to my Movies Archive!</h1>
-            <p>This is the Home Page</p>
-        </div>
+            <h2>This is the Home Page</h2>
+            <Link className="hover:text-indigo-500" to={'/movies/'}> Discover our Archive</Link>
+
+        </div >
     )
 }
